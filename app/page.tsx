b88c2620632897a697d401c6695974fd1e25c8bd
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2025-03-14 13:53:28 +0100

    Updated layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import type { SearchParams, Video } from "@/types";
 
-import { Link } from "@heroui/link";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
 import { title } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import { VideosGrid } from "@/components/VideosGrid";
 
 export default async function Home({
@@ -29,31 +24,9 @@ export default async function Home({
   const videos: Video[] = await res.json();
 
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col items-center justify-center gap-10 py-4 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
         <h1 className={title()}>Video Collection</h1>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
       </div>
       <VideosGrid videos={videos} />
     </section>

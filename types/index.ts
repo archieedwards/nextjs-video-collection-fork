4a0f9e9773d4287ac934b5070402522e914c4906
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2025-03-14 10:36:06 +0100

    Added data and types

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,31 @@
 import { SVGProps } from "react";
 
+import { videoTags } from "@/data/videoTags";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
+};
+
+export type SortOption = "created_at" | "title";
+export type SortDirection = "asc" | "desc";
+
+export type VideoTag = (typeof videoTags)[number];
+export type ThumbnailUrl =
+  `https://picsum.photos/seed/video${number}/${number}/${number}`;
+export type Video = {
+  id: string;
+  title: string;
+  thumbnail_url: ThumbnailUrl;
+  created_at: string;
+  duration: number;
+  views: number;
+  tags: VideoTag[];
+};
+
+export type SearchParams = {
+  searchTerm?: string;
+  sort?: SortOption;
+  direction?: SortDirection;
+  since?: string;
+  before?: string;
+  tags?: VideoTag[];
 };

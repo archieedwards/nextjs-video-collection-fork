377ae0259895919d78997d7ee58fbd56e0cb90ee
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2025-03-14 11:36:17 +0100

    Added videos grid component

---
+++ components/VideosGrid.tsx
@@ -0,0 +1,17 @@
+import type { Video } from "@/types";
+
+import { VideoCard } from "./VideoCard";
+
+interface VideosGridProps {
+  videos: Video[];
+}
+
+export function VideosGrid({ videos }: VideosGridProps) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      {videos.map((video) => (
+        <VideoCard key={video.id} video={video} />
+      ))}
+    </div>
+  );
+}
